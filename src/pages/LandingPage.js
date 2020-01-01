ffab3280102063454bdf97c63ffeedaed10564fb
Nova Laurente <novalaurente@gmail.com>
2020-01-01 21:55:39 +0800

    Add pulse effect in landing page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -9,7 +9,7 @@ const LandingPage = () => {
           <div id="triangles-container">
             <div id="triangles">
               <img className="animated zoomIn" id="circle-triangle" src="assets/images/triangles/circle-triangle.svg" alt="" />     
-              <div id="left-orange-container" className="container grow">
+              <div id="left-orange-container" className="container grow animated infinite pulse duration-5s delay-4s">
                 <img className="animated zoomIn image" id="left-orange" src="assets/images/triangles/left-orange.svg" alt="" />
                 <div className="middle left-orange-middle">
                   <Link to='content#aboutMe' smooth className='text text-white left-orange-text'>
@@ -20,7 +20,7 @@ const LandingPage = () => {
               <img className="animated zoomIn" id="blue-outline" src="assets/images/triangles/blue-outline.svg" alt="" />
               <img className="animated zoomIn" id="left-black-outline" src="assets/images/triangles/left-black-outline.svg" alt="" />
               
-              <div id="yellow-triangle-container" className="container grow">
+              <div id="yellow-triangle-container" className="container grow animated infinite pulse duration-5s delay-4s">
                 <img className="animated zoomIn image" src="assets/images/triangles/yellow.svg" alt="" />
                 <div className="middle yellow-triangle-middle">
                   <Link to='content#Skills' smooth className='text text-black yellow-triangle-text'>
@@ -29,7 +29,7 @@ const LandingPage = () => {
                 </div>
               </div>
 
-              <div id="black-triangle-container" className="container grow">
+              <div id="black-triangle-container" className="container grow animated infinite pulse duration-5s delay-4s">
                 <img className="animated zoomIn image" src="assets/images/triangles/black.svg" alt="" />
                 <div className="middle black-triangle-middle">
                   <Link to='content#Portfolio' smooth className='text text-white black-triangle-text'>
@@ -40,7 +40,8 @@ const LandingPage = () => {
               
               <img className="animated zoomIn" id="orange-outline" src="assets/images/triangles/orange-outline.svg" alt="" />
               <img className="animated zoomIn" id="blue-triangle" src="assets/images/triangles/blue.svg" alt="" />
-              <div id="right-orange-container" className="container grow">
+              
+              <div id="right-orange-container" className="container grow animated infinite pulse duration-5s delay-4s">
                 <img className="animated zoomIn image" src="assets/images/triangles/right-orange.svg" alt="" />
                 <div className="middle right-orange-middle">
                   <Link to='content#contactMe' smooth className='text text-white right-orange-text'>
